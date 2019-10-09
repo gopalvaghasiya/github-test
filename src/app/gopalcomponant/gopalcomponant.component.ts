1b0b6c984a479d7553fda43c886af73b6570d3bc
gopal <gopalv@hire4higherconsulting.com>
2019-10-09 14:25:40 +0530

    gopal componant added into branch

---
+++ src/app/gopalcomponant/gopalcomponant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gopalcomponant',
+  templateUrl: './gopalcomponant.component.html',
+  styleUrls: ['./gopalcomponant.component.css']
+})
+export class GopalcomponantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
